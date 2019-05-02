f98020bec8c0bae189d6acb4cb9671b5b0e3922f
jjshawshaw <jshaw00@stuy.edu>
2019-05-01 20:48:19 -0400

    works but poor ratios

--- MyHeap.java
+++ MyHeap.java
@@ -2,38 +2,31 @@ import java.util.*;
 public class MyHeap{
   private static void pushDown(int[] data, int size, int idx){
     //System.out.println("pushing down " + data[idx] + " at index " + idx);
-    //System.out.println(Arrays.toString(data));
+    //System.out.println(HeapPrinter.toString(data, size- 1));
     int c1 = idx * 2 + 1;
-    if (c1 >= size) return;
-    if (data[c1] > data[idx]){
-      //System.out.println("child1 " + c1 + " is larger");
-      swap(data, c1, idx);
-      pushDown(data, size, c1);
+    int c2 = idx * 2 + 2;
+    if (c1 >= size - 1) return;
+    int larger = (c2 >= size || data[c2] > data[c1]) ? c2 : c1;
+    if (data[larger] > data[idx]){
+      //System.out.println("child " + larger + " is larger");
+      swap(data, larger, idx);
+      pushDown(data, size, larger);
       pushUp(data, size, idx);
       return;
-    }
-    int c2 = idx * 2 + 2;
-    if (c2 >= size) return;
-    if (data[c2] > data[idx]){
-      //System.out.println("child2 " + c2 + " is larger");
-      swap(data, c2, idx);
-      pushDown(data, size, c2);
-      pushUp(data, size, idx);
     }
   }
 
   private static void pushUp(int[] data, int size, int idx){
     //System.out.println("pushing up " + data[idx] + " at index " + idx);
-    //System.out.println(Arrays.toString(data));
+    //System.out.println(HeapPrinter.toString(data, size - 1));
     int p = (idx % 2 == 0) ? idx / 2 - 1 : idx / 2;
-    if (idx == 0){
-      pushDown(data, size, idx);
-    }
+    if (idx == 0) return;
     if (p < 0) return;
     if (data[p] < data[idx]){
       swap(data, p, idx);
       pushUp(data, size, p);
       pushDown(data, size, idx);
+      return;
     }
   }
 
@@ -45,17 +38,13 @@ public class MyHeap{
     }
   }
 
-  private static void heapsort(int[] data){
+  public static void heapsort(int[] data){
     heapify(data);
-    int size = data.length - 1;
-    while (size > 0){
-      //System.out.println("\n" + size + " " + Arrays.toString(data));
-      swap(data, 0, size);
-      //System.out.println("swapping " + data[0] + " with " + data[size]);
-      //System.out.println("" + Arrays.toString(data));
-      //System.out.println("pushing down " + data[0] + "\n");
-      pushDown(data, size--, 0);
+    for (int i = data.length - 1; i > 0; i--){
+      swap(data,0,i);
+      pushDown(data, i, 0);
     }
+    if (data.length > 1 && data[0] > data[1]) swap(data, 0, 1);
   }
 
   private static void swap(int[] data, int idx1,int idx2){
@@ -66,40 +55,40 @@ public class MyHeap{
   }
 
   public static void main(String[]args){
-  System.out.println("Size\t\tMax Value\tmerge /builtin ratio ");
-  int[]MAX_LIST = {1000000000,500,10};
-  for(int MAX : MAX_LIST){
-    for(int size = 31250; size < 2000001; size*=2){
-      long qtime=0;
-      long btime=0;
-      //average of 5 sorts.
-      for(int trial = 0 ; trial <=5; trial++){
-        int []data1 = new int[size];
-        int []data2 = new int[size];
-        for(int i = 0; i < data1.length; i++){
-          data1[i] = (int)(Math.random()*MAX);
-          data2[i] = data1[i];
+    System.out.println("Size\t\tMax Value\tmerge /builtin ratio ");
+      int[]MAX_LIST = {1000000000,500,10};
+      for(int MAX : MAX_LIST){
+        for(int size = 31250; size < 2000001; size*=2){
+          long qtime=0;
+          long btime=0;
+          //average of 5 sorts.
+          for(int trial = 0 ; trial <=5; trial++){
+            int []data1 = new int[size];
+            int []data2 = new int[size];
+            for(int i = 0; i < data1.length; i++){
+              data1[i] = (int)(Math.random()*MAX);
+              data2[i] = data1[i];
+            }
+            long t1,t2;
+            t1 = System.currentTimeMillis();
+            MyHeap.heapsort(data2);
+            t2 = System.currentTimeMillis();
+            qtime += t2 - t1;
+            t1 = System.currentTimeMillis();
+            Arrays.sort(data1);
+            t2 = System.currentTimeMillis();
+            btime+= t2 - t1;
+            if(!Arrays.equals(data1,data2)){
+              System.out.println("FAIL TO SORT!");
+              System.exit(0);
+            }
+          }
+          System.out.println(size +"\t\t"+MAX+"\t"+1.0*qtime/btime);
         }
-        long t1,t2;
-        t1 = System.currentTimeMillis();
-        MyHeap.heapsort(data2);
-        t2 = System.currentTimeMillis();
-        qtime += t2 - t1;
-        t1 = System.currentTimeMillis();
-        Arrays.sort(data1);
-        t2 = System.currentTimeMillis();
-        btime+= t2 - t1;
-        if(!Arrays.equals(data1,data2)){
-          System.out.println("FAIL TO SORT!");
-          System.exit(0);
-        }
+        System.out.println();
       }
-      System.out.println(size +"\t\t"+MAX+"\t"+1.0*qtime/btime);
     }
-    System.out.println();
-  }
-  // int[] d = new int[]{};
+  // int[] d = new int[]{9, 8, 8, 8, 7, 1, 2, 0, 6, 0};
   // heapsort(d);
   // System.out.println(Arrays.toString(d));
-}
 }
