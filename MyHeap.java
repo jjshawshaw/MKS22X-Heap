b5a366641c16bf5c1ad741d082ebdf01dc64db6c
jjshawshaw <jshaw00@stuy.edu>
2019-05-01 21:41:05 -0400

    realizedd error was in main, sort working

--- MyHeap.java
+++ MyHeap.java
@@ -26,10 +26,9 @@ public class MyHeap{
     }
   }
 
-  public static void heapify(int[] data){
-    int x = (data.length - 2) / 2;
-    for (int s = x; s > -1; s--){
-      pushDown(data, data.length, s);
+  private static void heapify(int[] data){
+    for (int i = 0; i < data.length; i++){
+      pushUp(data, data.length, i);
     }
   }
 
@@ -50,42 +49,37 @@ public class MyHeap{
   }
 
   public static void main(String[]args){
-  System.out.println("Size\t\tMax Value\tmerge /builtin ratio ");
-  int[]MAX_LIST = {10,500,10};
-  for(int MAX : MAX_LIST){
-    for(int size = 10; size < 40; size*=2){
-      long qtime=0;
-      long btime=0;
-      //average of 5 sorts.
-      for(int trial = 0 ; trial <=5; trial++){
-        int []data1 = new int[size];
-        int []data2 = new int[size];
-        for(int i = 0; i < data1.length; i++){
-          data1[i] = (int)(Math.random()*MAX);
-          data2[i] = data1[i];
+    System.out.println("Size\t\tMax Value\tmerge /builtin ratio ");
+    int[]MAX_LIST = {1000000000,500,10};
+    for(int MAX : MAX_LIST){
+      for(int size = 31250; size < 2000001; size*=2){
+        long qtime=0;
+        long btime=0;
+        //average of 5 sorts.
+        for(int trial = 0 ; trial <=5; trial++){
+          int []data1 = new int[size];
+          int []data2 = new int[size];
+          for(int i = 0; i < data1.length; i++){
+            data1[i] = (int)(Math.random()*MAX);
+            data2[i] = data1[i];
+          }
+          long t1,t2;
+          t1 = System.currentTimeMillis();
+          MyHeap.heapsort(data2);
+          t2 = System.currentTimeMillis();
+          qtime += t2 - t1;
+          t1 = System.currentTimeMillis();
+          Arrays.sort(data1);
+          t2 = System.currentTimeMillis();
+          btime+= t2 - t1;
+          if(!Arrays.equals(data1,data2)){
+            System.out.println("FAIL TO SORT!");
+            System.exit(0);
+          }
         }
-        long t1,t2;
-        t1 = System.currentTimeMillis();
-        MyHeap.heapsort(data2);
-        t2 = System.currentTimeMillis();
-        qtime += t2 - t1;
-        t1 = System.currentTimeMillis();
-        Arrays.sort(data1);
-        t2 = System.currentTimeMillis();
-        btime+= t2 - t1;
-        if(!Arrays.equals(data1,data2)){
-          System.out.println(Arrays.toString(data1));
-          System.out.println(Arrays.toString(data2));
-          System.out.println("FAIL TO SORT!");
-          System.exit(0);
-        }
+        System.out.println(size +"\t\t"+MAX+"\t"+1.0*qtime/btime);
       }
-      System.out.println(size +"\t\t"+MAX+"\t"+1.0*qtime/btime);
+      System.out.println();
     }
-    System.out.println();
   }
-  // int[] d = new int[]{9, 8, 8, 8, 7, 1, 2, 0, 6, 0};
-  // heapsort(d);
-  // System.out.println(Arrays.toString(d));
-}
 }
